5d4beb927a94e14d5f8709e4fb6be052574f94fa
Allenn21 <diegovegabohorquez03@gmail.com>
2024-04-10 22:48:25 -0500

    break en Switch

--- funciones.js
+++ funciones.js
@@ -2,19 +2,25 @@ function calculadora(num1, num2, operador) {
     switch(operador) {
         case 'suma':
             return num1 + num2;
+            break;
         case 'resta':
             return num1 - num2;
+            break;
         case 'multiplicacion':
             return num1 * num2;
+            break;
         case 'division':
             if (num2 !== 0) {
                 return num1 / num2;
             } else {
                 return "No se puede dividir entre 0";
             }
+            break;
         case 'modulo':
             return num1%num2;
+            break;
         default:
             return "Elige una opción";
+            break;
     }
 }
